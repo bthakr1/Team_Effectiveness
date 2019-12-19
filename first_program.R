54e2e6b2041dab05c0a746f59cf403d4cf13375e
bthakr1 <44014462+bthakr1@users.noreply.github.com>
2019-12-19 10:20:59 -0600

    reading file

--- first_program.R
+++ first_program.R
@@ -1,3 +1,7 @@
 # Importing data set
 
 sarah_powers_data_hbap <- read.csv("~/Library/Mobile Documents/com~apple~CloudDocs/Harvard/LPA/Week 4/sarah_powers_data_hbap.csv")
+
+# Reading head 
+
+head(sarah_powers_data_hbap)

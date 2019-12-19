7a546219fd1313de3b53fd8280f892b75e379b76
bthakr1 <44014462+bthakr1@users.noreply.github.com>
2019-12-19 10:26:38 -0600

    new column to identify sales and non sales people

--- first_program.R
+++ first_program.R
@@ -5,3 +5,8 @@ sarah_powers_data_hbap <- read.csv("~/Library/Mobile Documents/com~apple~CloudDo
 # Reading head 
 
 head(sarah_powers_data_hbap)
+
+# Creating new variable to idetify sales and non sales people
+
+sarah_powers_data_hbap$sales_non_sales <- ifelse(sarah_powers_data_hbap$Sales_dept == 0, 'Non_Sales', 'Sales')
+

3300a870ca16c72cdcc6c6031ff1072bb42b0cfe
bthakr1 <44014462+bthakr1@users.noreply.github.com>
2019-12-19 17:16:03 -0600

    added multiple libraries

--- python_repo.py
+++ python_repo.py
@@ -1,5 +1,10 @@
-import pandas as pd 
+# importing modules for analysis
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns 
+import numpy as np
+
+# Reading the csv file 
 
 read_file = pd.read_csv("sarah_powers_data_hbap.csv")
 
-print(read_file.describe())

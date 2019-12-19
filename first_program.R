4c97ec1999c94b2f0b95c8747a1eaf8add06127e
bthakr1 <44014462+bthakr1@users.noreply.github.com>
2019-12-19 10:50:35 -0600

    dplyr introduced

--- first_program.R
+++ first_program.R
@@ -10,3 +10,19 @@ head(sarah_powers_data_hbap)
 
 sarah_powers_data_hbap$sales_non_sales <- ifelse(sarah_powers_data_hbap$Sales_dept == 0, 'Non_Sales', 'Sales')
 
+# Counting and plotting bar chart to identify sales vs non sales people
+
+counts <- table(sarah_powers_data_hbap$sales_non_sales)
+
+barplot(counts, main = "Sales vs Non Sales People", xlab = "Number of People")
+
+library(dplyr)
+
+# To get stats on total email count
+
+group_by(sarah_powers_data_hbap,sales_non_sales) %>% 
+  summarise(
+    count = n(),
+    mean = mean(emailcount, na.rm = TRUE),
+    median = median(emailcount, na.r=TRUE)
+  )
